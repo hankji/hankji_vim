187a62e84b80db823cf55e4d2c4dda9e1faba997
hankji <jihaiming@megvii.com>
2022-06-17 16:34:49 +0800

    add signature

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -47,6 +47,7 @@ cmp.setup({
     -- { name = 'ultisnips' }, -- For ultisnips users.
     -- { name = 'snippy' }, -- For snippy users.
     { name = 'buffer' },
+    { name = 'nvim_lsp_signature_help' },
   }
 })
 
